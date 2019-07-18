1a080cb38166322cb11bcd6bb3e3452b966920f2
GagandeepBajwa <32624560+GagandeepBajwa@users.noreply.github.com>
2019-07-18 00:08:40 -0700

    First Commit. Made chanegs to the ProductScreen.js and StyledTextInput.js

--- screens/home/ProductScreen.js
+++ screens/home/ProductScreen.js
@@ -1,128 +1,148 @@
-import React, { Component } from 'react';
-import {
-  ScrollView,
-  StyleSheet,
-  View,
-  Image,
-  Text,
-} from 'react-native';
+import React, { Component } from "react";
+import { ScrollView, StyleSheet,View,Image,Text,Button,TouchableHighlight,Dimensions} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import MainButton from '../../components/theme/MainButton';
+import MainButton from "../components/MainButton";
+import firebase from '../Firebase.js';
+import { Item } from "native-base";
 
-export class ProductScreen extends Component {
-  render() {
-    let pics = {
-      uri:
-        'https://cdn.pixabay.com/photo/2016/12/15/15/18/golf-1909115_960_720.jpg'
-    };
+export default class TestScreen extends Component {
 
-    return (
-      <View style={styles.container}>
-        <View style={styles.pictures}>
-          <Image
-            source={pics}
-            style={{ height: 350, width: 250, borderRadius: 5 }}
-          />
-        </View>
-
-        <View style={styles.infotext}>
-          <Text style={styles.productName}>
-            Golf clubs and bag only used once
-          </Text>
-
-          <View style={styles.LocViewAndPrice}>
-            <View style={styles.productLocView}>
-              <Ionicons name='ios-pin' size={18} />
-              <Text style={styles.productLoc}>Sahali, Kamloops</Text>
-            </View>
-
-            <View style={styles.priceDr}>
-              <Text style={styles.price}>2.5$</Text>
-              <Ionicons name='ios-car' size={18} />
-            </View>
-          </View>
-
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </Text>
-        </View>
-
-        <View style={styles.BottomPart}>
-          <MainButton title='Add to cart for 125$' />
-        </View>
-      </View>
-    );
+ constructor(props) {
+   super(props);
+   this.ref = firebase.firestore().collection('Products').doc(this.props.id);
+   this.state = {
+     pictures:[],
+     data: {}
+   } 
+   this.ref.onSnapshot(doc => {
+     this.setState({
+       pictures: doc.data().Pictures,
+       data: doc.data()
+     });
+   });
   }
+  
+ render() {
+   return (
+    
+     <View style={styles.container}>
+     <View style={styles.pictures}>
+       <ScrollView
+         horizontal={true}
+         pagingEnabled={true}
+         showsHorizontalScrollIndicator={false}
+       >
+           <Image source={{ uri: this.state.pictures[0] }} style={styles.pics}/>
+       </ScrollView>
+
+         </View>
+
+         <View style={styles.infotext}>
+
+         <Text style={styles.productName}>{this.state.data.Name}</Text>
+
+         <View style={styles.LocViewAndPrice}>
+
+         <View style={styles.productLocView}>
+           <Ionicons name='ios-pin' size={18}/><Text style ={styles.productLoc}>Sahali, Kamloops</Text>
+         </View>
+
+         <View style={styles.priceDr}>
+         <Text style={styles.price}>2.5$</Text>
+           <Ionicons name='ios-car' size={18}/>
+         </View>
+
+         </View>
+         <Text>{this.state.data.Description} </Text>
+         </View>
+
+
+
+         <View style={styles.BottomPart}>
+
+           <MainButton
+           title={'Add to cart for ' + this.state.data.Price +'$' }
+
+           />
+
+
+         </View>
+
+
+      </View>
+ );
 }
+}
+
+
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 10,
+    flex:10,
     paddingTop: 20,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
-  pictures: {
-    flex: 6,
-    alignItems: 'center'
+  pics : {
+    height: 350,
+    width: Dimensions.get('window').width,
+    borderRadius: 5,
   },
-
-  infotext: {
-    flex: 2.5,
-    paddingLeft: 8,
-    paddingRight: 5
+  pictures:{
+    flex:6,
+    alignItems:'center',
   },
-
-  productName: {
-    fontSize: 18,
-    fontWeight: '500'
+  infotext:{
+    flex:2.5,
+    paddingLeft:8,
+    paddingRight:5,
   },
-
-  productLocView: {
-    flexDirection: 'row',
-    flex: 0.9,
-    alignItems: 'flex-start'
+  productName:{
+    fontSize:18,
+    fontWeight:'500',
   },
-  productLoc: {
-    fontSize: 18,
-    fontWeight: '500',
-    paddingLeft: 7
+  productLocView:{
+    flexDirection:'row',
+    flex:0.9,
+    alignItems:'flex-start',
   },
-  price: {
-    fontSize: 18,
-    fontWeight: '500',
-    paddingRight: 7
+  productLoc:{
+    fontSize:18,
+    fontWeight:'500',
+    paddingLeft:7,
   },
-  priceDr: {
-    flex: 0.2,
-    flexDirection: 'row',
-    alignItems: 'flex-end'
+  price:{
+    fontSize:18,
+    fontWeight:'500',
+    paddingRight:7,
   },
-  LocViewAndPrice: {
-    paddingTop: 15,
-    paddingBottom: 15,
-    flexDirection: 'row'
+  priceDr:{
+    flex:0.2,
+    flexDirection:'row',
+    alignItems:'flex-end'
   },
-  BottomPart: {
-    flex: 1,
-    alignItems: 'center',
-    paddingBottom: 10,
-    paddingTop: 10
+  LocViewAndPrice:{
+    paddingTop:15,
+    paddingBottom:15,
+    flexDirection:'row',
   },
-
-  purchaseButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 300,
-    height: 50,
-    borderRadius: 14,
-    backgroundColor: 'rgb(57, 124,255)'
+  BottomPart:{
+    flex:1,
+    alignItems:'center',
+    paddingBottom:10,
+    paddingTop:10,
   },
-  buttonText: {
-    color: 'white',
-    fontSize: 15,
-    fontWeight: '800'
-  }
+  purchaseButton:{
+    alignItems:'center',
+    justifyContent:'center',
+    width:300,
+    height:50,
+    borderRadius:14,
+    backgroundColor:'rgb(57, 124,255)',
+  },
+  buttonText:{
+    color:'white',
+    fontSize:15,
+    fontWeight:'800',
+  },
 });
