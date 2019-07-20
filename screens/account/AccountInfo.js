3521d4ec734b8130ff82a16f4cf387c48ef1b76a
RocketJam <rocketjam3@gmail.com>
2019-07-20 22:21:10 +0600

    Account Info just created

---
+++ screens/account/AccountInfo.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { ScrollView, StyleSheet,View,Image,Text,Button,TouchableHighlight,Dimensions} from 'react-native';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import MainButton from "../../components/theme/MainButton"; //components\theme\MainButton.js
+import Colors from "../../constants/Colors.js";
+import firebase from '../../Firebase.js';
+import { Item } from "native-base";
+
+export class AccountInfo extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return null;
+    }
+
+    
+}
+  
+const styles = StyleSheet.create({
+
+        
+})
