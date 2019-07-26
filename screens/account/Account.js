d3f11e363c6fcde036fbbe8940ceb5888f338f31
GagandeepBajwa <32624560+GagandeepBajwa@users.noreply.github.com>
2019-07-26 13:21:03 -0700

    Post product working. Will be working on screen onverlay and auth

--- screens/account/Account.js
+++ screens/account/Account.js
@@ -2,8 +2,100 @@ import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import MainButton from '../../components/theme/MainButton';
 import Header from '../../components/headerComponents/Header';
-
+import firebase from '../../Firebase';
 export default class AccountScreen extends React.Component {
+  isUserEqual = (googleUser, firebaseUser) => {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (
+          providerData[i].providerId ===
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()
+        ) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  onSignIn = googleUser => {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged(
+      function(firebaseUser) {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!this.isUserEqual(googleUser, firebaseUser)) {
+          // Build Firebase credential with the Google ID token.
+          var credential = firebase.auth.GoogleAuthProvider.credential(
+            googleUser.idToken,
+            googleUser.accessToken
+          );
+          // Sign in with credential from the Google user.
+          firebase
+            .auth()
+            .signInAndRetrieveDataWithCredential(credential)
+            .then(result => {
+              console.log('user signed in ');
+              if (result.additionalUserInfo.isNewUser) {
+                firebase
+                  .database()
+                  .ref('/users/' + result.user.uid)
+                  .set({
+                    gmail: result.user.email,
+                    profile_picture: result.additionalUserInfo.profile.picture,
+                    first_name: result.additionalUserInfo.profile.given_name,
+                    last_name: result.additionalUserInfo.profile.family_name,
+                    created_at: Date.now()
+                  })
+                  .then(function(snapshot) {
+                    // console.log('Snapshot', snapshot);
+                  });
+              } else {
+                firebase
+                  .database()
+                  .ref('/users/' + result.user.uid)
+                  .update({
+                    last_logged_in: Date.now()
+                  });
+              }
+            })
+            .catch(function(error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+              // ...
+            });
+        } else {
+          console.log('User already signed-in Firebase.');
+        }
+      }.bind(this)
+    );
+  };
+  signInWithGoogleAsync = async () => {
+    try {
+      const result = await Expo.Google.logInAsync({
+        // androidClientId: YOUR_CLIENT_ID_HERE,
+        behavior: 'web',
+        iosClientId: '', //enter ios client id
+        scopes: ['profile', 'email']
+      });
+      if (result.type === 'success') {
+        this.onSignIn(result);
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  };
   render() {
     return (
       <View style={{ height: '100%' }}>
@@ -12,24 +104,33 @@ export default class AccountScreen extends React.Component {
           title='Go to Change Password Screen'
           onPress={() => this.props.navigation.navigate('ChangePassword')}
         />
-         <Button
+        <Button
           title='Go to Change Login Screen'
           onPress={() => this.props.navigation.navigate('Login')}
         />
- <Button
+        <Button
           title='Go to Change Sign Up Screen'
           onPress={() => this.props.navigation.navigate('SignUp')}
         />
-        
+        <Button
+          title='Sign In With Google'
+          onPress={() => this.signInWithGoogleAsync()}
+        />
         <View style={styles.viewStyle}>
-          <MainButton title='Login' onPress={() => this.props.navigation.navigate('Login')}/>
-          <MainButton secondary='true' title='Sign Up' onPress={() => this.props.navigation.navigate('SignUp')}/>
+          <MainButton
+            title='Login'
+            onPress={() => this.props.navigation.navigate('Login')}
+          />
+          <MainButton
+            secondary='true'
+            title='Sign Up'
+            onPress={() => this.props.navigation.navigate('SignUp')}
+          />
         </View>
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   viewStyle: {
     flex: 1,
@@ -39,4 +140,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   }
-});
+})
