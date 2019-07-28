fa3185389df2c15b734d8104a120936dde774f36
GagandeepBajwa <32624560+GagandeepBajwa@users.noreply.github.com>
2019-07-27 18:36:27 -0700

    facebook auth working with expo. I have to add the facebook login in firebase and create an app on facebook-for-developers to make it work

--- screens/testScreens/Test.js
+++ screens/testScreens/Test.js
@@ -1,219 +1,84 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
-import { View, Button, Text, TextInput, Image } from 'react-native';
+import { View, Button, Text, TextInput, Image, StyleSheet } from 'react-native';
 
-import firebase from 'react-native-firebase';
+import firebase from '../../Firebase';
+import { Facebook } from 'expo';
+
 
 const successImageUri = 'https://cdn.pixabay.com/photo/2015/06/09/16/12/icon-803718_1280.png';
 
 export default class TestScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.unsubscribe = null;
-    this.state = {
-      user: null,
-      message: '',
-      codeInput: '',
-      phoneNumber: '+1',
-      confirmResult: null,
-    };
-
-  }
-  componentDidMount() {
-    this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ user: user.toJSON() });
-      } else {
-        // User has been signed out, reset the state
-        this.setState({
-          user: null,
-          message: '',
-          codeInput: '',
-          phoneNumber: '+1',
-          confirmResult: null,
-        });
-      }
-    });
-  }
-
-  componentWillUnmount() {
-     if (this.unsubscribe) this.unsubscribe();
-  }
-
-
-  signIn = () => {
-    const { phoneNumber } = this.state;
-    this.setState({ message: 'Sending code ...' });
-
-    var appVerifier = this.recaptchaVerifier;
-    firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
-      .then(confirmResult => this.setState({ confirmResult, message: 'Code has been sent!' }))
-      .catch(error => this.setState({ message: `Sign In With Phone Number Error: ${error.message}` }));
-  };
-
-  confirmCode = () => {
-    const { codeInput, confirmResult } = this.state;
-
-    if (confirmResult && codeInput.length) {
-      confirmResult.confirm(codeInput)
-        .then((user) => {
-          this.setState({ message: 'Code Confirmed!' });
-        })
-        .catch(error => this.setState({ message: `Code Confirm Error: ${error.message}` }));
-    }
-  };
-
-  signOut = () => {
-    firebase.auth().signOut();
-  }
-
-  renderPhoneNumberInput() {
-   const { phoneNumber } = this.state;
-
-    return (
-      <View style={{ padding: 25 }}>
-        <Text>Enter phone number:</Text>
-        <TextInput
-          autoFocus
-          style={{ height: 40, marginTop: 15, marginBottom: 15 }}
-          onChangeText={value => this.setState({ phoneNumber: value })}
-          placeholder={'Phone number ... '}
-          value={phoneNumber}
-        />
-        <Button title="Sign In" color="green" onPress={this.signIn} />
-      </View>
-    
-    );
-  }
-
-  renderMessage() {
-    const { message } = this.state;
-
-    if (!message.length) return null;
-
-    return (
-      <Text style={{ padding: 5, backgroundColor: '#000', color: '#fff' }}>{message}</Text>
-    );
-  }
-
-  renderVerificationCodeInput() {
-    const { codeInput } = this.state;
-
-    return (
-      <View style={{ marginTop: 25, padding: 25 }}>
-        <Text>Enter verification code below:</Text>
-        <TextInput
-          autoFocus
-          style={{ height: 40, marginTop: 15, marginBottom: 15 }}
-          onChangeText={value => this.setState({ codeInput: value })}
-          placeholder={'Code ... '}
-          value={codeInput}
-        />
-        <Button title="Confirm Code" color="#841584" onPress={this.confirmCode} />
-      </View>
-    );
-  }
-
-  render() {
-    const { user, confirmResult } = this.state;
-    return (
-      <View style={{ flex: 1 }}>
-
-        {!user && !confirmResult && this.renderPhoneNumberInput()}
-
-        {this.renderMessage()}
-
-        {!user && confirmResult && this.renderVerificationCodeInput()}
-
-        {user && (
-          <View
-            style={{
-              padding: 15,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: '#77dd77',
-              flex: 1,
-            }}
-          >
-            <Image source={{ uri: successImageUri }} style={{ width: 100, height: 100, marginBottom: 25 }} />
-            <Text style={{ fontSize: 25 }}>Signed In!</Text>
-            <Text>{JSON.stringify(user)}</Text>
-            <Button title="Sign Out" color="red" onPress={this.signOut} />
-          </View>
-        )}
-      </View>
-    );
-  }
-}
-=======
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import * as GoogleSignIn from 'expo-google-sign-in';
-import {AppAuth} from 'expo-app-auth';
-
-
-export default class TestScreen extends React.Component{
   state = { user: null };
-
+ 
+  FacebookApiKey= "2872116616149463";
   componentDidMount() {
-    this.initAsync();
-    console.log("componentDidMount working");
+    // List to the authentication state
+    this._unsubscribe = firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
   }
-
-  initAsync = async () => {  
-    try{
-      await GoogleSignIn.initAsync({
-        clientId: '572236256696-m327o3i0d6qvb73qu366hqmhvb47v38f.apps.googleusercontent.com'
-      });
-    }
-    catch({message}){
-      alert('GoogleSignIn.initAsync():  '+ message );
-    }
-    console.log('initAsync Working!');
-    this._syncUserWithStateAsync();
-  };
-
-  _syncUserWithStateAsync = async () => {
-    const user = await GoogleSignIn.signInSilentlyAsync();
+ 
+  componentWillUnmount() {
+    // Clean up: remove the listener
+    this._unsubscribe();
+  }
+ 
+  onAuthStateChanged = user => {
+    // if the user logs in or out, this will be called and the state will update.
+    // This value can also be accessed via: firebase.auth().currentUser
     this.setState({ user });
   };
-
-  signOutAsync = async () => {
-    await GoogleSignIn.signOutAsync();
-    this.setState({ user: null });
-  };
-
-  signInAsync = async () => {
-    try {
-
-      await GoogleSignIn.askForPlayServicesAsync();
-      const { type, user } = await GoogleSignIn.signInAsync();
-      if (type === 'success') {
-        this._syncUserWithStateAsync();
-      }
-    } catch ({ message }) {
-      alert('login: Error:' + message);
-    }
-  };
-
-  onPress = () => {
-    if (this.state.user) {
-      this.signOutAsync();
+ 
+  async facebookLogin() {
+    // Get an API Key from https://developers.facebook.com/ (it's pretty easy)
+    const authData = await Facebook.logInWithReadPermissionsAsync(this.FacebookApiKey);
+    if (!authData) return;
+    const { type, token } = authData;
+    if (type === 'success') {
+      return token;
     } else {
-      this.signInAsync();
+      // Maybe the user cancelled...
+    }
+  }
+ 
+  loginAsync = async () => {
+    // First we login to facebook and get an "Auth Token" then we use that token to create an account or login. This concept can be applied to github, twitter, google, ect...
+    const token = await this.facebookLogin();
+    if (!token) return;
+    // Use the facebook token to authenticate our user in firebase.
+    const credential = firebase.auth.FacebookAuthProvider.credential(token);
+    try {
+      // login with credential
+      await firebase.auth().signInAndRetrieveDataWithCredential(credential);
+    } catch ({ message }) {
+      alert(message);
     }
   };
-
+ 
+  async logoutAsync() {
+    try {
+      await firebase.auth().signOut();
+    } catch ({ message }) {
+      alert(message);
+    }
+  }
+ 
+  toggleAuth = () => {
+    if (!!this.state.user) {
+      this.logoutAsync();
+    } else {
+      this.loginAsync();
+    }
+  };
+ 
   render() {
+    const { user } = this.state;
+    const message = !!user ? 'Logout' : 'Login';
     return (
-    <View style={styles.viewStyle} >
-    <Text  onPress={this.onPress}>Toggle Auth</Text>
-
-    </View>
+      <View style={styles.viewStyle}>
+        <Text onPress={this.toggleAuth}>{message}</Text>
+      </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   viewStyle: {
     flex: 1,
@@ -224,5 +89,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 })
-
->>>>>>> expo-google-sign-in
