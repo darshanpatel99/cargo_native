aeefd6bbcd824cab2fce412b5740fb9631bed5da
GagandeepBajwa <32624560+GagandeepBajwa@users.noreply.github.com>
2019-07-27 22:39:22 -0700

    saving some unsaved changes

--- screens/testScreens/Test.js
+++ screens/testScreens/Test.js
@@ -1,79 +1,143 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import * as GoogleSignIn from 'expo-google-sign-in';
-import {AppAuth} from 'expo-app-auth';
+import React, { Component } from 'react';
+import { View, Button, Text, TextInput, Image } from 'react-native';
 
+import firebase from '../../Firebase';
 
-export default class TestScreen extends React.Component{
-  state = { user: null };
+const successImageUri = 'https://cdn.pixabay.com/photo/2015/06/09/16/12/icon-803718_1280.png';
 
-  componentDidMount() {
-    this.initAsync();
-    console.log("componentDidMount working");
+export default class TestScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.unsubscribe = null;
+    this.state = {
+      user: null,
+      message: '',
+      codeInput: '',
+      phoneNumber: '+1',
+      confirmResult: null,
+    };
   }
 
-  initAsync = async () => {  
-    try{
-      await GoogleSignIn.initAsync({
-        clientId: '572236256696-m327o3i0d6qvb73qu366hqmhvb47v38f.apps.googleusercontent.com'
-      });
-    }
-    catch({message}){
-      alert('GoogleSignIn.initAsync():  '+ message );
-    }
-    console.log('initAsync Working!');
-    this._syncUserWithStateAsync();
-  };
-
-  _syncUserWithStateAsync = async () => {
-    const user = await GoogleSignIn.signInSilentlyAsync();
-    this.setState({ user });
-  };
-
-  signOutAsync = async () => {
-    await GoogleSignIn.signOutAsync();
-    this.setState({ user: null });
-  };
-
-  signInAsync = async () => {
-    try {
-
-      await GoogleSignIn.askForPlayServicesAsync();
-      const { type, user } = await GoogleSignIn.signInAsync();
-      if (type === 'success') {
-        this._syncUserWithStateAsync();
+  componentDidMount() {
+    this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user: user.toJSON() });
+      } else {
+        // User has been signed out, reset the state
+        this.setState({
+          user: null,
+          message: '',
+          codeInput: '',
+          phoneNumber: '+1',
+          confirmResult: null,
+        });
       }
-    } catch ({ message }) {
-      alert('login: Error:' + message);
+    });
+  }
+
+  componentWillUnmount() {
+     if (this.unsubscribe) this.unsubscribe();
+  }
+
+  signIn = () => {
+    const { phoneNumber } = this.state;
+    this.setState({ message: 'Sending code ...' });
+
+    firebase.auth().signInWithPhoneNumber(phoneNumber)
+      .then(confirmResult => this.setState({ confirmResult, message: 'Code has been sent!' }))
+      .catch(error => this.setState({ message: `Sign In With Phone Number Error: ${error.message}` }));
+  };
+
+  confirmCode = () => {
+    const { codeInput, confirmResult } = this.state;
+
+    if (confirmResult && codeInput.length) {
+      confirmResult.confirm(codeInput)
+        .then((user) => {
+          this.setState({ message: 'Code Confirmed!' });
+        })
+        .catch(error => this.setState({ message: `Code Confirm Error: ${error.message}` }));
     }
   };
 
-  onPress = () => {
-    if (this.state.user) {
-      this.signOutAsync();
-    } else {
-      this.signInAsync();
-    }
-  };
+  signOut = () => {
+    firebase.auth().signOut();
+  }
+
+  renderPhoneNumberInput() {
+   const { phoneNumber } = this.state;
+
+    return (
+      <View style={{ padding: 25 }}>
+        <Text>Enter phone number:</Text>
+        <TextInput
+          autoFocus
+          style={{ height: 40, marginTop: 15, marginBottom: 15 }}
+          onChangeText={value => this.setState({ phoneNumber: value })}
+          placeholder={'Phone number ... '}
+          value={phoneNumber}
+        />
+        <Button title="Sign In" color="green" onPress={this.signIn} />
+      </View>
+    );
+  }
+
+  renderMessage() {
+    const { message } = this.state;
+
+    if (!message.length) return null;
+
+    return (
+      <Text style={{ padding: 5, backgroundColor: '#000', color: '#fff' }}>{message}</Text>
+    );
+  }
+
+  renderVerificationCodeInput() {
+    const { codeInput } = this.state;
+
+    return (
+      <View style={{ marginTop: 25, padding: 25 }}>
+        <Text>Enter verification code below:</Text>
+        <TextInput
+          autoFocus
+          style={{ height: 40, marginTop: 15, marginBottom: 15 }}
+          onChangeText={value => this.setState({ codeInput: value })}
+          placeholder={'Code ... '}
+          value={codeInput}
+        />
+        <Button title="Confirm Code" color="#841584" onPress={this.confirmCode} />
+      </View>
+    );
+  }
 
   render() {
+    const { user, confirmResult } = this.state;
     return (
-    <View style={styles.viewStyle} >
-    <Text  onPress={this.onPress}>Toggle Auth</Text>
+      <View style={{ flex: 1 }}>
 
-    </View>
+        {!user && !confirmResult && this.renderPhoneNumberInput()}
+
+        {this.renderMessage()}
+
+        {!user && confirmResult && this.renderVerificationCodeInput()}
+
+        {user && (
+          <View
+            style={{
+              padding: 15,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: '#77dd77',
+              flex: 1,
+            }}
+          >
+            <Image source={{ uri: successImageUri }} style={{ width: 100, height: 100, marginBottom: 25 }} />
+            <Text style={{ fontSize: 25 }}>Signed In!</Text>
+            <Text>{JSON.stringify(user)}</Text>
+            <Button title="Sign Out" color="red" onPress={this.signOut} />
+          </View>
+        )}
+      </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  viewStyle: {
-    flex: 1,
-    flexDirection: 'column',
-    height: '100%',
-    alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
-
