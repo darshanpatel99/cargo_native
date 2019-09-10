0b5d7b04cb26cc8742c909b749becf42f7a2ef67
Darshan <darshanpatel1902@gmail.com>
2019-09-09 21:54:09 -0700

    updated categories

--- components/category/RefineCategoryHomeScreen.js
+++ components/category/RefineCategoryHomeScreen.js
@@ -6,66 +6,20 @@ import Colors from '../../constants/Colors'
 const items = [
   // this is the parent or 'item'
   {
-    name: 'Categories',
-    id: 0,
-    // these are the children or 'sub items'
-    children: [
-      {
-        name: 'Clothing',
-        id: 1,
-      },
-      {
-        name: 'Furniture',
-        id: 2,
-      },
-      {
-        name: 'Sporting Goods, Exercise',
-        id: 3,
-      },
-      {
-        name: 'Home-Indoor',
-        id: 4,
-      },
-      {
-        name: 'Art, Collectables',
-        id: 5,
-      },
-      {
-        name: 'Home Appliances',
-        id: 6,
-      },
-      {
-        name: 'Baby Items',
-        id: 7,
-      },
-      {
-        name: 'Toys, Games',
-        id: 8,
-      },
-      {
-        name: 'Electronics',
-        id: 9,
-      },
-      {
-        name: 'Phones',
-        id: 10,
-      },
-      {
-        name: 'Computers',
-        id: 11,
-      },
-      {
-        name: 'Books',
-        id: 12,
-      },
-      {
-        name: 'Other',
-        id: 13,
-      },
-    ],
+    name: 'Electronics',
+    id: 1,
 
   },
- 
+
+  {
+    name: 'Home Products',
+    id: 2,
+  },
+
+  {
+    name: 'Baby Products',
+    id: 3,
+  }
 
 ];
 
@@ -94,7 +48,7 @@ export default class RefineCategoryHomeScreen extends Component {
           subKey="children"
           selectText="Refine"
           showDropDowns={true}
-          readOnlyHeadings={true}
+          readOnlyHeadings={false}
           onSelectedItemsChange={this.onSelectedItemsChange}
           selectedItems={this.state.selectedItems}
           single={false}
