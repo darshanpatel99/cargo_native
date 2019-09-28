db25dcde0f98166f3921c8fd785c4c193d6e8152
Sachin <sachin@Sachins-MacBook-Pro.local>
2019-09-28 11:03:14 -0700

    removed unnecessary files and added permissions in app.json

--- screens/addProduct/PostProductScreen.js
+++ screens/addProduct/PostProductScreen.js
@@ -116,7 +116,7 @@ export default class PostProductScreen extends Component {
 
 
 
-    this.getPermissionAsync();
+    //this.getPermissionAsync();
     this._unsubscribe = firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
     console.log('component did mount');
   }
@@ -180,16 +180,16 @@ export default class PostProductScreen extends Component {
       downloadURLs : [],
       addressArray:[],
       uploadCounter:0,
-      firstTimeOnly:true,
     });
     navigate('Home');
   };
 
   getPermissionAsync = async () => {
     if (Constants.platform.ios) {
+      console.log('ask permission');
       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL && Permissions.CAMERA);
       if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to post the product!');
+        alert('Sorry, we need camera roll permissions to make this work!');
       }
     }
   };
@@ -212,27 +212,20 @@ export default class PostProductScreen extends Component {
 
   //Uploading all the product related stuff
   uploadImageData =  async () =>{
-    
+    this.setState({ loading: true });
       var array = this.state.image; //getting the uri array
-      var first = this.state.firstTimeOnly;
-      console.log("Total number of uris we have"+ array.length)
-      this.setState({ loading: true });
+    
       array.forEach(async (element) => {
 
-   
-        if(first){
-
-          first=false;
-          this.setState({firstTimeOnly:false});
+        if(this.state.firstTimeOnly){
           await this.uploadThumbnailToFirebase(element)
-          .then(()=>{   
-            
+          .then(()=>{
             console.log('Thumbnail got uploaded');
+            
           })
           .catch(error=>{
             console.log("Hey there is an error:  " +error);
           });
-          
         }
 
         await this.uploadImageToFirebase(element, uuid.v1())
@@ -256,43 +249,12 @@ export default class PostProductScreen extends Component {
 
   //start post the add button
   startPostTheProduct = async () =>{
-    let titleLength = this.state.title;
-    let priceLength = parseInt( this.state.price);
-    let descriptionLength = this.state.description;
-    let productCategory = this.state.Category;
-    let picArray = this.state.image;
-    let timeArray = this.state.Avability;
-    let address = this.state.googleAddressEmpty;
-    if(titleLength.length > 0 && priceLength >= 10 && priceLength <= 1000 && descriptionLength.length > 0 && productCategory !=0 && picArray.length>0 && timeArray.length>0 && address != '')  {
     await this.uploadImageData();
-    }
-    else {
-      console.log('hello');
-  
-      if((priceLength < 10 || priceLength > 1000) && picArray.length!=0){
-        this.setState({
-          priceAlert:true,
-        })      
-      }
-      else if(timeArray.length==0 && picArray.length!=0 && (priceLength >= 10 || priceLength <= 1000)){
-        this.setState({
-          availableAlert:true,
-        })
-      }
-  
-      console.log(address)
-  
-      if(address == '' && picArray.length!=0 && timeArray.length!=0 && (priceLength >= 10 || priceLength <= 1000)){
-        this.setState({
-          showAddressAlert:true,
-        })
-      }
-  
-      this.setState({
-        postAdClicked: true,
-      })
-    }
   }
+
+
+
+
 
 
   //post the product
@@ -336,7 +298,6 @@ export default class PostProductScreen extends Component {
       DeliveryFee:'',
       TotalFee:'',
       BoughtStatus:'false',
-      OrderNumber: -1,
 
     }
 
@@ -347,13 +308,12 @@ export default class PostProductScreen extends Component {
     //Posting the product
     PostProduct(data).then(()=>{
       this.setState({ loading: false });
-      this.showAlert2();
     });
     console.log("Product Posted---->" + data);
 
     //change the overlay visibility to visible
     //this.setState({isOverlayVisible:true});
-   
+    this.showAlert2();
 
 
   } else {
@@ -394,6 +354,8 @@ export default class PostProductScreen extends Component {
    * Function Description:
    */
   _pickImage = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    const {status_roll} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality:0.2,
@@ -439,9 +401,13 @@ export default class PostProductScreen extends Component {
    */
     
   _pickImageCamera = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    const {status_roll} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality:0.2      
+      quality:0.2
+      //allowsEditing: true,
+      
     });
     
     console.log(result);
@@ -486,7 +452,7 @@ export default class PostProductScreen extends Component {
 
     const manipResult = await ImageManipulator.manipulateAsync(
       uri,
-      [{ resize:{width:400, height:400} }],
+      [{ resize:{width:200, height:200} }],
       { compress: 0.5, format: ImageManipulator.SaveFormat.JPEG }
     )
 
@@ -787,7 +753,7 @@ export default class PostProductScreen extends Component {
     console.log('test fucntion');
     checkGoogleAddress = 'lalalals'
     console.log(checkGoogleAddress)
-    this.state.googleAddressEmpty = '';
+    this.state.googleAddressEmpty = text;
     //this.setState({googleAddressEmpty: 'test'})
     
   }
@@ -803,14 +769,11 @@ export default class PostProductScreen extends Component {
     if(this.state.User != null){
       return (
         <View style={{flex:1}}>
-
         <Spinner
-          visible={this.state.loading}
-          textContent={'Loading...'}
-          textStyle={styles.spinnerTextStyle}
-        />
-
-        
+            visible={this.state.loading}
+            textContent={'Loading...'}
+            textStyle={styles.spinnerTextStyle}
+          />
         <KeyboardAvoidingView
 
           style={{ flex: 1 }}
@@ -936,12 +899,6 @@ export default class PostProductScreen extends Component {
                     // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
                 }}
 
-                GooglePlacesSearchQuery={{
-                  // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                  rankby: 'distance',
-                  input :'address',
-                  circle: '5000@50.676609,-120.339020',
-                }}
 
                
 
@@ -957,8 +914,6 @@ export default class PostProductScreen extends Component {
                     region: 'Canada',
                     radius: 20000,
                     strictbounds: true,
-
-                    types: 'address', // default: 'geocode'
                 }}
 
                 styles={{
@@ -1003,7 +958,7 @@ export default class PostProductScreen extends Component {
             show={showAlert2}
             showProgress={false}
             title="Alert"
-            message={'Successfully Posted!!\n'}
+            message={'This is warning 1  \n This is warning 2 \n This is warning 3 '}
             closeOnTouchOutside={false}
             closeOnHardwareBackPress={false}
             //showCancelButton={true}
@@ -1134,6 +1089,9 @@ const styles = {
   mainConatiner: {
     flex: 1
   },
+  spinnerTextStyle: {
+    color: '#0000FF'
+  },
   imageUploadStyle: {
     height: 100,
     width: width/2 - 15,
@@ -1188,11 +1146,6 @@ const styles = {
     marginTop: 5,
     //alignItems:'center'
   },
-  
-  spinnerTextStyle: {
-    color: '#0000FF'
-  },
-
 
   errorStyle:{
     borderColor:'red',
