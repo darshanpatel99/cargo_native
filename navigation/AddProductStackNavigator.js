e6a158fab31710f22f2d68b70e5a23e1d443a60e
Darshan <darshanpatel1902@gmail.com>
2019-09-30 00:52:09 +0000

    Merge branch 'master' into 'master'
    Updtaed expo version and dependencies
    
    See merge request meetcargo/cargo-native-v1!123
    
    (cherry picked from commit d088418fd4d58f42ab4de8b9dc0b474ef9f0ef21)

--- navigation/AddProductStackNavigator.js
+++ navigation/AddProductStackNavigator.js
@@ -1,6 +1,4 @@
-import {
-  createStackNavigator
-} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import PostProductScreen from '../screens/addProduct/PostProductScreen';
 
 export default AddProductStack = createStackNavigator({
